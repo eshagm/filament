f5b1f5637d1f3d2c3b745e76433d633122e21c03
eshagm <131762914+eshagm@users.noreply.github.com>
2023-05-29 15:58:56 +0530

    filament(29-5)

---
+++ filament1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+main()
+{
+	char a[100],b[100];
+	printf("enter string =");
+	gets(a);
+	strcpy(b,a);
+	strrev(b);
+	if(strcmp(a,b)==0)
+	{
+		printf("this string is palindrome");
+	}
+	else
+	{
+		printf("this string is not palindrome");
+	}
+			
+}
